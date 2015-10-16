c323143ffb424cdf422f49783c7e6ba0af1eedce
Ashley Luhrs <aluhrs@gmail.com>
2015-10-15 18:21:57 -0700

    fix immediately grabbing from cache bug

--- README.md
+++ README.md
@@ -1,16 +1,18 @@
 # Transparent Caching Proxy Server
 
-##For testing the test suite:
+##Setting Up the Project
 
 1. Clone the project
 2. ```bundle install```
-3. ```rspec spec/caching_proxy_server_spec.rb```
 
-##To manually test:
+##Automated Unit Testing
+1. ```rspec spec/caching_proxy_server_spec.rb```
+
+##Manual Integration Testing:
 
 1. Run the program - ```ruby init.rb``` It will prompt you, but the information is also below.
-2. Open a browser
-3. Type in localhost:2000/maps (or anything after the slash). It should respond with a response as well as 'Received response from server:'
+2. Open a browser.
+3. Type in localhost:2000/one (or anything after the slash). It should respond with a response as well as 'Received response from server:'
 4. After the / type in anything else. It should respond with a response as well as 'Received response from server:'
 5. Type in /maps (or the first thing you typed again). It should respond with a response as well as 'You've hit the cache.'
 
@@ -27,3 +29,6 @@ The cache is set up as so:
                               }
         }
 ```
+
+Extra TODOs:
+1) Use Net::HTTP for automated integration testing.
--- caching_proxy_server.rb
+++ caching_proxy_server.rb
@@ -13,41 +13,56 @@ class CachingProxyServer
     @ordered_urls = []
     @cache_configuration = {
                               cache_duration: 30 * 1000, # seconds
-                              cache_size_bytes: 1024 * 2, # total size of cache in bytes
+                              cache_size_bytes: 1024 * 1000, # total size of cache in bytes
                               cache_size_elements: 2 # total of elements in cache
                             }
   end
 
-  # Open a server connection and accept requests.
+  # Open a server connection and accept requests
   def start_server
     server = TCPServer.new 2000
     response = nil
+    source_time = nil
     loop do
       client = server.accept
-      puts "new client: #{client.inspect}"
       url = client.gets
-      pathname = parse_url(url)
-      # favicon is returned on every request. it's noisy, so let's ignore it.
-      unless pathname == '/favicon.ico'
-        puts "pathname: #{pathname}"
-        hashed_pathname = hash_pathname(pathname)
-        cached_response = get_cached_version(hashed_pathname)
-        if cached_response
-          response = "You've hit the cache. The response from #{pathname} is: #{cached_response}"
-        else
-          response = "Received response from #{pathname}: #{fetch_from_source(pathname)}"
-          store_data(pathname, response, hashed_pathname)
+      if url && proceed?(source_time)
+        pathname = parse_url(url)
+        # Favicon is returned on every request. It's noisy, so let's ignore it.
+        unless pathname == '/favicon.ico'
+          hashed_pathname = hash_pathname(pathname)
+          cached_response = get_cached_version(hashed_pathname)
+          if cached_response
+            response = "You've hit the cache. The response from #{pathname} is: #{cached_response}"
+          else
+            response = "Received response from #{pathname}: #{fetch_from_source(pathname)}"
+            store_data(pathname, response, hashed_pathname)
+          end
         end
       end
 
       client.puts response
       client.close
+      puts "closing client"
+      source_time = Time.now.to_i
     end
   end
 
+  # An issue was happening where after correctly grabbing the response from the
+  # destination source and adding it to the cache, the server accepted another
+  # request almost immediately. This was causing the 'Received from cache to
+  # be printed to the browser' and you would never see the initial 'Received
+  # from destination source' message in the browser. For testing puproses, to
+  # ensure you still see the 'Received from destination source' message in the
+  # browser, adding logic to display the response saved rather than then
+  # grabbing from the cache if it's been less than a second since grabbing from
+  # the destination source.
+  def proceed?(source_time)
+    current_time = Time.now.to_i
+    source_time ? ((current_time - source_time) > 1) : true
+  end
+
   def get_cached_version(pathname)
-    puts "looking for pathname in cache: #{pathname}"
-    puts "getting cache: #{cache.keys}"
     if cache[pathname]
       current_time = Time.now.to_i
       if (current_time - cache[pathname][:date_stored]) < cache_configuration[:cache_duration]
@@ -57,7 +72,6 @@ class CachingProxyServer
   end
 
   def fetch_from_source(pathname)
-    puts "fetching from source"
     # For testing purposes, test against google
     url = "http://www.google.com#{pathname}"
     parsed_url = URI.parse(url)
@@ -84,22 +98,21 @@ class CachingProxyServer
     add_to_ordered_pathnames(hashed_pathname)
   end
 
-  # get pathname
+  # Get pathname
   def parse_url(url)
     url = url.split(' ')
     url[1]
   end
 
+  # Hash the pathname for security purposes
   def hash_pathname(pathname)
     Digest::MD5.hexdigest(pathname)
   end
 
   def make_space(response)
     if !room_for_more_elements?
-      puts "not enough room for element"
       delete_from_cache
     elsif !enough_space?(response)
-      puts "not enough space"
       while !enough_space?(response) do
         delete_from_cache
       end
@@ -107,7 +120,6 @@ class CachingProxyServer
   end
 
   def delete_from_cache
-    puts "deleting from cache"
     url_to_delete = ordered_urls.shift
     url_to_delete_size = cache[url_to_delete][:size]
     @cache_size -= url_to_delete_size
@@ -123,7 +135,6 @@ class CachingProxyServer
   end
 
   def add_to_cache(url, response)
-    puts "adding to cache"
     cache[url] = {
                     date_stored: Time.now.to_i,
                     size: response.bytesize,
@@ -132,15 +143,13 @@ class CachingProxyServer
   end
 
   def update_cache_size(response)
-    puts "updating cache size"
     @cache_size += response.bytesize
   end
 
-  # Keeping an ordered list of the pathnames so that when need to remove from
+  # Storing an ordered list of the pathnames so that when need to remove from
   # cache, to get the oldest item, I can simply take the first item in this list
   # then look it up in the cache and remove it.
   def add_to_ordered_pathnames(pathname)
-    puts "adding to ordered urls"
     ordered_urls << pathname
   end
 end
