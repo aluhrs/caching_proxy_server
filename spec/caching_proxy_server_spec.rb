798e363dc105b27efc752abbc9cf2b46becc5cfa
Ashley Luhrs <aluhrs@gmail.com>
2015-10-29 17:29:22 -0400

    remove unnecessary specs

--- spec/caching_proxy_server_spec.rb
+++ spec/caching_proxy_server_spec.rb
@@ -2,15 +2,6 @@ require_relative '../caching_proxy_server.rb'
 
 describe CachingProxyServer do
   let(:server) { CachingProxyServer.new }
-  let(:cached_pathname) { Digest::MD5.hexdigest('/maps') }
-
-  before(:each) do
-    server.cache[cached_pathname] = {
-                                      date_stored: Time.now.to_i,
-                                      size: 1024,
-                                      response: "This the response",
-                                    }
-  end
 
   describe "#parse_url" do
     it "returns the pathname of the url" do
